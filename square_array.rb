0d5b2f5f75e77b0d7dc43783da2b1b87908cf6be
Kelley Chaplain <kmchaplain@gmail.com>
2019-04-12 03:36:45 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,7 +1,8 @@
 def square_array(array)
   array = [1, 2, 3]
-    array.each do {|n| new_array = (n*n)}
-      return new_array
+    array.each do |n| 
+      new_array = (n*n)
+  return new_array
       
   end    
 end
